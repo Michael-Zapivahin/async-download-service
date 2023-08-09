b55fb9b1b75db06216ea7a219e5fd7e929766bb4
Michael-Zapivahin <zapivahin@ya.ru>
2023-08-09 13:12:02 +0300

    download archive

--- server.py
+++ server.py
@@ -1,9 +1,62 @@
 from aiohttp import web
 import aiofiles
+import datetime
+import asyncio
+import logging
+import os
 
 
-async def archive(request):
-    raise NotImplementedError
+INTERVAL_SECS = 1
+
+
+async def archive(request, read_up_bytes=102400):
+    archive_hash = request.match_info['archive_hash']
+    photos_filepath = os.path.join('test_photos', archive_hash)
+    process = await asyncio.create_subprocess_exec(
+        'zip',
+        '-r',
+        '-',
+        '..',
+        stdout=asyncio.subprocess.PIPE,
+        cwd=photos_filepath,
+    )
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = f'attachment; filename={archive_hash}.zip'
+    await response.prepare(request)
+    try:
+        while not process.stdout.at_eof():
+            zip_binary = await process.stdout.read(read_up_bytes)
+            await response.write(zip_binary)
+            await asyncio.sleep(1)
+        return response
+    except asyncio.CancelledError:
+        logger.info('Download was interrupted')
+        raise
+    finally:
+        if process.returncode is None:
+            process.kill()
+            await process.communicate()
+            logger.info('Process was killed')
+
+
+async def uptime_handler(request):
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
 
 
 async def handle_index_page(request):
@@ -13,6 +66,9 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(logging.StreamHandler())
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
